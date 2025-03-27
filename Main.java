84897bc0d651bf2b35bf6188cd18a5adcfa18e15
Shivasheesh Chatterjee <chatterjee.shivasheesh@gmail.com>
2025-03-28 00:59:35 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Java prog");
+        System.out.println("output from this");
+        System.out.println(new Date());
+    }
+}
